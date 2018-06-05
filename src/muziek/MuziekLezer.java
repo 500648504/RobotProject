910268ce46894e9a290f98cb32cf1d8108e55652
Jorg Vrensen <vrensej@hva.nl>
2018-06-05 16:42:08 +0200

    Einde van dinsdag 5-6 laatste wijzigingen

--- src/muziek/MuziekLezer.java
+++ src/muziek/MuziekLezer.java
@@ -10,54 +10,36 @@ import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class MuziekLezer {
-	
+
 	private Brick brick;
-	float []sample;
-	
-	
-	public MuziekLezer (Brick brick) {
+	float[] sample;
+
+	public MuziekLezer(Brick brick) {
 		this.brick = brick;
 	}
-	
+
 	public ArrayList<Integer> leesMuziek() {
-	EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
-	
-	SampleProvider kleurLezer = sensor.getColorIDMode();
-	SampleProvider kleurLezer = new autoAdjustFilter(kleurlezer);
-	
-	int sampleSize = reflectedLight.sampleSize();
-    float[] sample = new float[sampleSize];
-    
-	int sampleteller = 0;
-	ArrayList <Integer> sampleLijst = new ArrayList<>(); 
-	    while (Button.ESCAPE.isUp()) {
-	      
-	    	/*
-	       * Get a fresh sample from the filter. (The filter gets it from its
-	       * source, the kleurLezer)
-	       */
-	      kleurLezer.fetchSample(sample, 0);
-	      int currentSample = (Math.round(sample[sampleteller]));
-	      sampleLijst.add(currentSample);
-	      System.out.println(currentSample);
-	      
-	      
-	      
-	      
-	      /* Display individual values in the sample. */	      
-//	      for (int i = 0; i < sample.length; i++) {
-//	        System.out.println(sample[i] + " ");
-//	        
-//	        
-//	      }
-	      sampleteller ++;
-	      Delay.msDelay(1000);
-	    }
-	    /* When ready close the sensor */
-	    sensor.close();
-	    return sampleLijst;
+		EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
+
+		int currentSample;
+		int sampleteller = 0;
+		
+		ArrayList<Integer> sampleLijst = new ArrayList<>();
+
+		while (Button.ESCAPE.isUp()) {
+			currentSample = sensor.getColorID();
+			System.out.println("test " + sampleteller + " " + sensor.getColorID());
+			sampleLijst.add(currentSample);
+			
+		
+
+		
+		sampleteller++;
+		Delay.msDelay(1000);
+		}
+		// When ready close the sensor */
+		sensor.close();
+		return sampleLijst;
 
 	}
 }
-
-
