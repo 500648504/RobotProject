fcf65b730566f837e0764563edb09fadee621416
Bobby Komen <bobby.komen@hva.nl>
2018-06-06 14:23:56 +0200

    Menu gemaakt en getest.

--- src/main/RobotLauncher.java
+++ src/main/RobotLauncher.java
@@ -4,42 +4,82 @@ import lejos.hardware.Button;
 import lejos.hardware.Key;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 import lijnvolgen.PIDController;
 import muziek.MuziekLezer;
 import tests.*;
 
-
 public class RobotLauncher {
 	
+		/*
+		 * Comments:
+		 * Het openen van locaties van objecten kun je doen door
+		 * met de cursos op het object te gaan staan, en de I aan te klikken.
+		 */
 	
-	//geeft lego ding een naam
-	Brick brick;
 	
-	//nieuwe robot constructorlego ding
-	public RobotLauncher() {
-		super();
-		brick = LocalEV3.get();
-	}
-	
-	//main method, nieuwe Marvin gedefinieerd en roept test aan
-	public static void main(String[] args) {
-		RobotLauncher deRijdendeRobot = new RobotLauncher();		
+		//geeft lego ding een naam
+		Brick brick;										// Variable aangemaakt, naam brick.
 		
+		// Nieuwe robot constructor, lego ding
+		// Constructor voor de main.
+		public RobotLauncher() {
+			super();
+			brick = LocalEV3.get();					// LocalEV 3 te benaderen met de cursor erop te staan en op C klikken
+		}
 		
-		
-
-		
-		
-		
-//		 Code: Bobby (Object PIDController)
-		PIDController pid = new PIDController(); 
-			pid.run(); 
-			
-			MuziekLezer MuziekReader = new MuziekLezer();
-			MuziekReader.testRun();
+		//main method, nieuwe Marvin gedefinieerd en roept test aan
+		public static void main(String[] args) {
+		RobotLauncher deRijdendeRobot = new RobotLauncher();			// Aanmaken object
+		deRijdendeRobot.startMenu();									// Openen menu
 		}
 	
+	
+		
+	public void startMenu() {
+		boolean menuLoop = true;
+		
+		System.out.println("Kies welk programma u wilt draaien:");
+		System.out.println ("Links: Lijn");
+		System.out.println ("Midden:  Muziek");
+		System.out.println ("Rechts: Volgt nog");
+		
+		
+		while (menuLoop) {
+			int selection = 0;
+			if(Button.LEFT.isDown())  selection = 1;
+			if(Button.ENTER.isDown()) selection = 2;
+			if(Button.RIGHT.isDown()) selection = 3;
+
+
+			switch (selection) {
+				case 1:
+					System.out.println ("Lijn volgen");
+					PIDController pid = new PIDController(); 				
+					while (true) {
+						if (Button.ESCAPE.isDown()) {
+							Motor.B.stop();
+							Motor.C.stop();
+							menuLoop = false;
+							break;
+						}
+					pid.run(); 
+					//Delay.msDelay(20);		//delay om hele snelle loop te voorkomen - minder chrashes?
+					} 
+					break; 
+				case 2:
+					MuziekLezer MuziekReader = new MuziekLezer();
+					MuziekReader.testRun();
+					break; 
+				case (3):
+					System.out.println ("Volgt nog");
+					break; 
+			}
+		}
+		System.out.println("Gestopt");
+		Delay.msDelay(1000);
+	}
 	
 	
 	
--- src/tests/Lichtsensortest.java
+++ src/tests/Lichtsensortest.java
@@ -1,57 +1,57 @@
-package tests;
-
-//nog aan geklooid.... werkt nu niet meer Jorg
-
-import lejos.hardware.Brick;
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.port.SensorPort;
-import lejos.robotics.Color;
-import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.lcd.TextLCD;
-
-public class Lichtsensortest {
-	
-		//deze methode heeft ook de brick nodig
-		private Brick brick;
-	
-		//constructor voor de test
-		public Lichtsensortest(Brick brick) {
-			this.brick = brick;
-		}
-
-
-	    //methode zelf
-		public void ambientLightTest () {
-			
-			//colorsensor naam geeft
-	        EV3ColorSensor colorsensor = new EV3ColorSensor(SensorPort.S3);
-	        SampleProvider colorsensorId = colorsensor.getColorIDMode();
-	        float[] current_sample;
-
-	        //dit stuk uit de code
-	        System.out.println("Color Demo");
-	        
-	        
-	        Button.LEDPattern(4);    // flash green led and
-	        Sound.beepSequenceUp();    // make sound when ready.
-
-	        Button.waitForAnyPress();
-	        Button.LEDPattern(0);
-	        
-	        
-	        // run until escape button pressed.	    	        
-	        while (Button.ESCAPE.isUp())		//loop zolang escape niet ingedrukt is
-	        {
-	            colorsensorId.fetchSample(current_sample, 0);
-	        	//LCD.clear(4);					//maak leeg, nog even precies uitzoeken?
-	            System.out.println("test " + current_sample.getColorID());
-	            Delay.msDelay(250);				//wacht 250 ms om loop niet te snel te maken
-	        }
-
-	        Delay.msDelay(1000);
-		}
-}
+//package tests;
+//
+////nog aan geklooid.... werkt nu niet meer Jorg
+//
+//import lejos.hardware.Brick;
+//import lejos.hardware.Button;
+//import lejos.hardware.Sound;
+//import lejos.hardware.port.SensorPort;
+//import lejos.robotics.Color;
+//import lejos.robotics.SampleProvider;
+//import lejos.utility.Delay;
+//import lejos.hardware.sensor.EV3ColorSensor;
+//import lejos.hardware.lcd.LCD;
+//import lejos.hardware.lcd.TextLCD;
+//
+//public class Lichtsensortest {
+//	
+//		//deze methode heeft ook de brick nodig
+//		private Brick brick;
+//	
+//		//constructor voor de test
+//		public Lichtsensortest(Brick brick) {
+//			this.brick = brick;
+//		}
+//
+//
+//	    //methode zelf
+//		public void ambientLightTest () {
+//			
+//			//colorsensor naam geeft
+//	        EV3ColorSensor colorsensor = new EV3ColorSensor(SensorPort.S3);
+//	        SampleProvider colorsensorId = colorsensor.getColorIDMode();
+//	        float[] current_sample;
+//
+//	        //dit stuk uit de code
+//	        System.out.println("Color Demo");
+//	        
+//	        
+//	        Button.LEDPattern(4);    // flash green led and
+//	        Sound.beepSequenceUp();    // make sound when ready.
+//
+//	        Button.waitForAnyPress();
+//	        Button.LEDPattern(0);
+//	        
+//	        
+//	        // run until escape button pressed.	    	        
+//	        while (Button.ESCAPE.isUp())		//loop zolang escape niet ingedrukt is
+//	        {
+//	            colorsensorId.fetchSample(current_sample, 0);
+//	        	//LCD.clear(4);					//maak leeg, nog even precies uitzoeken?
+//	            System.out.println("test " + current_sample.getColorID());
+//	            Delay.msDelay(250);				//wacht 250 ms om loop niet te snel te maken
+//	        }
+//
+//	        Delay.msDelay(1000);
+//		}
+//}
