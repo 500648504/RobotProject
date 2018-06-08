4a1ef16876f72a49fede08c055c300172c0ce297
Jorg Vrensen <vrensej@hva.nl>
2018-06-08 14:39:19 +0200

    update van mijn vorig commit over fysieke reacties

--- src/quizmaster/FysiekeReacties.java
+++ src/quizmaster/FysiekeReacties.java
@@ -1,5 +1,37 @@
 package quizmaster;
 
-public class FysiekeReacties {
+import lejos.hardware.motor.Motor;
 
+/**
+ * @author jvren
+ *
+ */
+
+public class FysiekeReacties {
+	
+	private final static int SHAKESPEED = 100;
+	private final static int SHAKEDISTANCE = 20;
+
+
+	public void schudHoofd(int aantal) {
+		Motor.B.setSpeed(SHAKESPEED);
+		
+		for (int draai = 0 ; draai < aantal ; aantal++) {
+		
+		Motor.B.resetTachoCount();
+		
+		while (Motor.B.getTachoCount() < SHAKEDISTANCE ) {
+			Motor.B.forward();
+		}
+		Motor.B.resetTachoCount();
+		
+		while (Motor.B.getTachoCount() < SHAKEDISTANCE )
+			Motor.B.backward();
+		}
+		
+	}
+	
+	
+	
+	
 }
